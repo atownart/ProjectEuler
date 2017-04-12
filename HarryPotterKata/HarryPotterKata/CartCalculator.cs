ef4fd0365037c7781957f63a51721e0c6c6c1b41
atownart <atownart@gmail.com>
2017-04-12 18:20:43 +0100

    added base project

---
+++ HarryPotterKata/HarryPotterKata/CartCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HarryPotterKata
+{
+    public class CartCalculator : ICartCalculator
+    {
+        public double Total(IDictionary<int, int> cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HarryPotterKata/HarryPotterKata/ICartCalculator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HarryPotterKata
+{
+    public interface ICartCalculator
+    {
+        double Total(IDictionary<int, int> cart);
+    }
+}
