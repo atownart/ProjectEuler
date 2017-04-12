a8ec7a61b4c514c1090ab2ee0a46ea041e3af511
atownart <atownart@gmail.com>
2017-04-12 19:16:10 +0100

    simple asserts

---
+++ HarryPotterKata/HarryPotterKata.Tests/CartCalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace HarryPotterKata.Tests
+{
+    [TestFixture]
+    public class CartCalculatorTests
+    {
+        private readonly CartCalculator _classUnderTest;
+
+        public CartCalculatorTests()
+        {
+            _classUnderTest = new CartCalculator();
+        }
+
+        [TestCase(1,8d)]
+        [TestCase(2, 15.2d)]
+        [TestCase(3, 21.6d)]
+        [TestCase(4, 25.6d)]
+        [TestCase(5, 30d)]
+        public void Total_AmountOfDifferentBooks_ExpectedDiscount(int amountDifferentBooks, double expectedTotal)
+        {
+            //Arrange 
+            var cart = new Dictionary<int,int>();
+            for (var i = 0; i < amountDifferentBooks; i++)
+            {
+                cart.Add(i +1,1);
+            }
+
+            //Act
+            var result = _classUnderTest.Total(cart);
+
+            //Assert
+            Assert.AreEqual(expectedTotal,result,$"Expecting total of {expectedTotal.ToString()} but got {result.ToString()}");
+        }
+    }
+}
--- HarryPotterKata/HarryPotterKata/CartCalculator.cs
+++ HarryPotterKata/HarryPotterKata/CartCalculator.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HarryPotterKata
 {
     public class CartCalculator : ICartCalculator
     {
+        private readonly double[] _discountPerBook;
+
+        private const double FullPrice = 8d;
+
+        public CartCalculator()
+        {
+            _discountPerBook = new double[5];
+            _discountPerBook[0] = 0;//1 book
+            _discountPerBook[1] = 0.05d;//2 book
+            _discountPerBook[2] = 0.1d;//3 book
+            _discountPerBook[3] = 0.2d;
+            _discountPerBook[4] = 0.25d;
+
+        }
+
         public double Total(IDictionary<int, int> cart)
         {
-            throw new NotImplementedException();
+            var total = cart.Count*FullPrice;
+            return total - (total * _discountPerBook[cart.Count - 1]);
         }
     }
 }
