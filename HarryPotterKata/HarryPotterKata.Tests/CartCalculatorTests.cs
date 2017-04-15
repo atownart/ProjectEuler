ab6e807eda9f0c02f9d46f8bf99d027b87ce25bb
atownart <atownart@gmail.com>
2017-04-15 20:01:37 +0100

    failing edge case test

--- HarryPotterKata/HarryPotterKata.Tests/CartCalculatorTests.cs
+++ HarryPotterKata/HarryPotterKata.Tests/CartCalculatorTests.cs
@@ -44,59 +44,17 @@ namespace HarryPotterKata.Tests
             var result = _classUnderTest.Total(cart);
 
             //Assert
-            Assert.AreEqual(expectedTotal, result, $"Expecting total of {expectedTotal.ToString()} but got {result.ToString()}");
+            Assert.AreEqual(expectedTotal, result, $"Expecting total with simple sets of {expectedTotal.ToString()} but got {result.ToString()}");
         }
 
-        //[Test]
-        //public void Total_3Books2Different_5PercentDiscounted()
-        //{
-        //    //Arrange
-        //    var cart = new Dictionary<int, int>() { { 1, 2 }, { 2, 1 }};
+        [TestCase(new int[] {1, 1, 2, 2, 3, 3, 4, 5}, 51.2d)]
+        public void Total_EdgeCases_FindsCheapestTotal(int[] cart, double expectedTotal)
+        {
+            //Act
+            var result = _classUnderTest.Total(cart);
 
-        //    //Act 
-        //    var result = _classUnderTest.Total(cart);
-
-        //    //Assert
-        //    Assert.AreEqual(23.2d, result);
-        //}
-
-        //[Test]
-        //public void Total_4Books2Different_5PercentDiscounted()
-        //{
-        //    //Arrange
-        //    var cart = new Dictionary<int, int>() { { 1, 2 }, { 2, 2 } };
-
-        //    //Act 
-        //    var result = _classUnderTest.Total(cart);
-
-        //    //Assert
-        //    Assert.AreEqual(30.4d, result);
-        //}
-
-        //[Test]
-        //public void Total_5Books2Different_5PercentDiscounted()
-        //{
-        //    //Arrange
-        //    var cart = new Dictionary<int, int>() { { 1, 2 }, { 2, 2 }, {3,1} };
-
-        //    //Act 
-        //    var result = _classUnderTest.Total(cart);
-
-        //    //Assert
-        //    Assert.AreEqual(36.8d, result);
-        //}
-
-        //[Test]
-        //public void Total_8Books3Different_10PercentDiscounted()
-        //{
-        //    //Arrange
-        //    var cart = new Dictionary<int, int>() { { 1, 2 }, {2,2}, {3,2}, {4,1}, {5,1} };
-
-        //    //Act 
-        //    var result = _classUnderTest.Total(cart);
-
-        //    //Assert
-        //    Assert.AreEqual(51.2d, result);
-        //}
+            //Assert
+            Assert.AreEqual(expectedTotal, result, $"Expecting total with edge case sets of {expectedTotal.ToString()} but got {result.ToString()}");
+        }
     }
 }
